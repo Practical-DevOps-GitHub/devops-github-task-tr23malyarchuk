07f4d76ab7436f322aa0057958116d86f110b33b
tr23malyarchuk <malyarchuk.bogdan@lll.kpi.ua>
2024-06-22 16:54:55 +0300

    returned default value in script_test.rb

--- .github/tests/test/script_test.rb
+++ .github/tests/test/script_test.rb
@@ -83,7 +83,7 @@ class ScriptTest < Test::Unit::TestCase
 
   def test_without_approval_main
     actual = @obj.rules_required_pull_request_reviews('main').nil? || @obj.rules_required_pull_request_reviews('main')["required_approving_review_count"]
-    expected = 1
+    expected = 0
     assert_equal(expected, actual, 'We shouldn\'t have any approvals before merge to main branch')
   end
 
